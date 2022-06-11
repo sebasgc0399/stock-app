70106f65870a6780f61bfbbf4de468bcd911d60a
Sebastian <sebasgc0399@gmail.com>
2022-06-10 22:42:37 -0500

    Conectamos MongoBD

--- app.js
+++ app.js
@@ -1,15 +1,47 @@
-const express=require('express')
+const express = require('express')
+const mongoose = require('mongoose')
+const path = require('path')
 require('dotenv').config()
 
-const app=express()
+const app = express()
 
-app.get('/',(req,res)=>{
-  console.log('Petición recibida')
-  res.status(200).send('<h1>Hola mundo!</h1>')
+console.log(process.env.MARTIN)
+
+mongoose
+  .connect(
+    `mongodb+srv://Sebasgc0399:${process.env.MONGO_DB_PASS}@development.mdixs.mongodb.net/?retryWrites=true&w=majority`
+  )
+  .then((result) => {
+    app.listen(PORT, () => {
+      console.log(`Servidor escuchando en el puerto ${PORT}`)
+    })
+    console.log('Conexión exitosa a la BBDD')
+  })
+  .catch((err) => console.log(err))
+
+const productSchema = mongoose.Schema(
+  {
+    name: { type: String, required: true },
+    price: Number,
+  },
+  { timestamps: true }
+)
+
+const Product = mongoose.model('Product', productSchema)
+
+app.use(express.json())
+
+app.post('/api/v1/products', (req, res) => {
+  const newProduct = new Product(req.body)
+
+  newProduct
+    .save()
+    .then((result) => {
+      res.status(201).json({ ok: true })
+    })
+    .catch((err) => console.log(err))
 })
 
-const PORT=process.env.PORT || 4000
+app.use(express.static(path.join(__dirname, 'public')))
 
-app.listen(PORT,()=>{
-    console.log(`Servidor corriendo en el puerto ${PORT}`)
-})
+const PORT = process.env.PORT
